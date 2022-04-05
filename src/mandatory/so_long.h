30e5d1eafb650d2e568ec22eabb21c623a3b55d2
Igor De spirlet <ide-spir@fu-r2-p7.s19.be>
2022-04-05 15:18:40 +0200

    Finishing the parsing of map structure

--- src/mandatory/so_long.h
+++ src/mandatory/so_long.h
@@ -6,7 +6,7 @@
 /*   By: ide-spir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 14:26:50 by ide-spir          #+#    #+#             */
-/*   Updated: 2022/04/05 11:57:35 by ide-spir         ###   ########.fr       */
+/*   Updated: 2022/04/05 15:15:12 by ide-spir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,31 @@
 # define A_LEFT 0
 # define D_RIGHT 2
 # define KEY_ESC 53
+
+//! Errors
+# define ERROR_MAP_NOT_FOUND 1
+# define ERROR_MAP_NOT_VALID 2
+# define ERROR_MAP_WIDTH 3
+# define ERROR_MAP_BORDER 4
+# define ERROR_MAP_PLAYER 5
+# define ERROR_MAP_EXIT 6
+# define ERROR_MAP_COINS 7
+# define ERROR_MAP_FORBIDDEN_ITEM 8
+# define ERROR_MAP_NOT_BER_EXTENSION 9
+# define ERROR_GAME_IS_NULL 10
+
+typedef struct s_game
+{
+	char	**map;
+	int		game;
+	int		count_move;
+	int		height;
+	int		width;
+	int		p_x;
+	int		p_y;
+	int		nbr_coins;
+	void	*mlx;
+	void	*mlx_win;
+}				t_game;
 
 #endif
