ae74200c36ee6ba933cdb535bf20538f874b7f7a
Igor De spirlet <ide-spir@fu-r2-p7.s19.be>
2022-04-25 13:55:03 +0200

    Modifying errors messages

--- src/bonus/errors_1to5_bonus.c
+++ src/bonus/errors_1to5_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ide-spir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 11:09:10 by ide-spir          #+#    #+#             */
-/*   Updated: 2022/04/19 13:30:59 by ide-spir         ###   ########.fr       */
+/*   Updated: 2022/04/25 13:50:57 by ide-spir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,35 @@
 
 void	got_error_map_not_found(void)
 {
-	ft_putstr_fd("Error : ", 1);
-	ft_putstr_fd("ERROR_MAP_NOT_FOUND\n", 1);
+	ft_putstr_fd("Error\n", 1);
+	ft_putstr_fd("ERROR MAP NOT FOUND\n", 1);
 	exit(ERROR_MAP_NOT_FOUND);
 }
 
 void	got_error_map_not_valid(void)
 {
-	ft_putstr_fd("Error : ", 1);
-	ft_putstr_fd("ERROR_MAP_NOT_VALID\n", 1);
+	ft_putstr_fd("Error\n", 1);
+	ft_putstr_fd("ERROR MAP NOT VALID\n", 1);
 	exit(ERROR_MAP_NOT_VALID);
 }
 
 void	got_error_map_width(void)
 {
-	ft_putstr_fd("Error :", 1);
-	ft_putstr_fd("ERROR_MAP_WIDTH\n", 1);
+	ft_putstr_fd("Error\n", 1);
+	ft_putstr_fd("ERROR MAP WIDTH\n", 1);
 	exit(ERROR_MAP_WIDTH);
 }
 
 void	got_error_map_border(void)
 {
-	ft_putstr_fd("Error : ", 1);
-	ft_putstr_fd("ERROR_MAP_BORDER\n", 1);
+	ft_putstr_fd("Error\n", 1);
+	ft_putstr_fd("ERROR MAP BORDER\n", 1);
 	exit(ERROR_MAP_BORDER);
 }
 
 void	got_error_map_player(void)
 {
-	ft_putstr_fd("Error : ", 1);
-	ft_putstr_fd("ERROR_MAP_PLAYER\n", 1);
+	ft_putstr_fd("Error\n", 1);
+	ft_putstr_fd("ERROR MAP PLAYER\n", 1);
 	exit(ERROR_MAP_PLAYER);
 }
